495fa3296a6b1838b32322cc4751907430130348
alok322618-jpg <alok322618@gmail.com>
2026-05-23 16:50:24 +0530

    Update CORS to allow all origins including file://

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import pricesRoutes from './routes/prices.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 // Health check
