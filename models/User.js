2e3a7d5dbf3ba725ac2a6c76e2c995438ad2e08d
alok322618-jpg <alok322618@gmail.com>
2026-06-14 11:50:16 +0530

    Fix 3: add name field to User model

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     passwordHash: { type: String, required: true },
     isVerified: { type: Boolean, default: false },
